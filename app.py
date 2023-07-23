117deebc98fa76c5d4161641c82519dd2b7ecfcd
i44_ <jsbbdjzjsjawk+@gmail.com>
2023-07-24 00:10:29 +0300

    startup with threads

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
+import logging
 import streamlit as st
-from djitellopy import Tello
+from djitellopy import Tello, TelloException
+from multiprocessing.pool import ThreadPool
 
 drones_ip = ['192.168.0.120', '192.168.0.121',
              '192.168.0.122', '192.168.0.123', '192.168.0.124']
 drones = list()
 
 
-@st.cache_data
-def connect(ip):
-    drone = Tello(ip)
-    drone.connect()
-    drone.streamon()
+def calculate_ports(ip):
+    "Первый - для STATE_UDP_PORT, второй - VS_UDP_PORT"
+    id = int(ip.split('.')[-1])
+    return str(9000 + id * 10), str(11111 + id * 10)
+
+
+def connect_drone(ip):
+    try:
+        state, vs = calculate_ports(ip)
+        drone = Tello(host=ip, vs_udp=vs)
+        drone.LOGGER.setLevel(logging.WARN)
+        drone.connect()
+        drone.set_network_ports(state, vs)
+        drone.streamon()
+    except TelloException:
+        drone = None
+        print(f"{ip}: Failed to connect to tello.")
     return drone
 
 
-for ip in drones_ip:
-    drones.append(connect(ip))
+@st.cache_data
+def connect_all(ips):
+    with ThreadPool(len(ips)) as pool:
+        return list(pool.map(connect_drone, ips))
+
+
+drones = connect_all(drones_ip)
 
 
 def page(index):
